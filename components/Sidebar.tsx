f0825b86c450050ca25b5416023d9135c38167ab
Saurabh Prajapati <saurabhprajapati511@gmail.com>
2024-01-16 11:37:29 +0530

    final commit

---
+++ components/Sidebar.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import React from "react";
+import { usePathname } from "next/navigation";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import { useMemo } from "react";
+import { twMerge } from "tailwind-merge";
+
+import Box from "./Box";
+import SidebarItem from "./SidebarItem";
+import Library from "./Library";
+
+import { Song } from "@/types";
+import usePlayer from "@/hooks/usePlayer";
+
+interface SidebarProps {
+    children: React.ReactNode;
+    songs: Song[];
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ children, songs }) => {
+
+    const pathname = usePathname();
+    const player = usePlayer();
+
+    const routes = useMemo(() => [
+        {
+            icon: HiHome,
+            label: "Home",
+            active: pathname !== "/search",
+            href: "/"
+        },
+        {
+            icon: BiSearch,
+            label: "Search",
+            active: pathname === "/search",
+            href: "/search"
+        }
+
+    ], [pathname])
+
+    return (
+        <div className={twMerge(
+            "flex h-full",
+            player.activeId && "h-[calc(100%-80px)]"
+        )}>
+            <div className="invisible hidden md:visible md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+                <Box>
+                    {routes.map((item) => {
+                        return (
+                            <SidebarItem
+                                key={item.label}
+                                {...item}
+                            />
+                        )
+                    })}
+                </Box>
+                <Box className="overflow-y-auto h-full">
+                    <Library songs={songs} />
+                </Box>
+            </div>
+            <main className="h-full flex-1 overflow-y-auto py-2">
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export default Sidebar
